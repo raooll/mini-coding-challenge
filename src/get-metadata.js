ae3afad265df82784c2a6e57d65bd1ccdd40ba3b
raooll <rahul.aug.1986@gmail.com>
2022-07-06 15:45:39 +0530

    complete get metadate function

--- src/get-metadata.js
+++ src/get-metadata.js
@@ -10,6 +10,104 @@ type Metadata = {
 };
 */
 
+const _ = require("lodash");
+
+/**
+ * Parse regex match results
+ * @param {string[]} results
+ * @returns {string | null} returns the matched string or null
+ */
+function parseResult(results) {
+  if (results && results.length >= 1) {
+    return results[1];
+  } else {
+    return null;
+  }
+}
+
+/**
+ *  Get title metadata
+ * @param {string} htmlString html content in string format
+ * @returns {string | null} returns the title from the metadata or null
+ */
+function getTitle(htmlString) {
+  const titleMatches = htmlString.match(/<title[^>]*>([^]*?)<\/title>/i);
+  return parseResult(titleMatches);
+}
+
+/**
+ *  Get url metadata
+ * @param {string} htmlString html content in string format
+ * @returns {string | null} returns the url from the metadata or null
+ */
+function getUrl(htmlString) {
+  const ogMatches = htmlString.match(
+    /<meta[\s]*?property="og:url"[.|\s]*?content="([^]*?)"(.|[\s])*?>/i
+  );
+  return parseResult(ogMatches);
+}
+
+/**
+ *  Get site name metadata
+ * @param {string} htmlString html content in string format
+ * @returns {string | null} returns the siteName from the metadata or null
+ */
+function getSiteName(htmlString) {
+  const matches = htmlString.match(
+    /<meta[\s]*?property="og:site_name"[.|\s]*?content="([^]*?)"(.|[\s])*?>/i
+  );
+  return parseResult(matches);
+}
+
+/**
+ * Get description metadata
+ * @param {string} htmlString html content in string format
+ * @returns {string | null} returns the description from the metadata or null
+ */
+function getDescription(htmlString) {
+  const ogMatches = htmlString.match(
+    /<meta[\s]*?property="og:description"[.|\s]*?content="([^]*?)"(.|[\s])*?>/i
+  );
+  const nameMatches = htmlString.match(
+    /<meta[\s]*?name="description"[.|\s]*?content="([^]*?)"(.|[\s])*?>/i
+  );
+
+  return parseResult(ogMatches) || parseResult(nameMatches);
+}
+
+/**
+ *  Get Keywords metadata
+ * @param {string} htmlString html content in string format
+ * @returns {string[] | null} returns the keyword array from the metadata or null
+ */
+function getKeywords(htmlString) {
+  const matches = htmlString.match(
+    /<meta[\s]*?name="keywords"[.|\s]*?content="([^]*?)"(.|[\s])*?>/i
+  );
+
+  const keywordsString = parseResult(matches);
+  if (keywordsString != null) {
+    return keywordsString
+      .split(",")
+      .filter((e) => e)
+      .map(_.trim);
+  } else {
+    return null;
+  }
+}
+
+/**
+ * Get Author metadata
+ * @param {string} htmlString html content in string format
+ * @returns {string | null} returns the author from the metadata or null
+ */
+function getAuthor(htmlString) {
+  const matches = htmlString.match(
+    /<meta[\s]*?name="author"[.|\s]*?content="([^]*?)"(.|[\s])*?>/i
+  );
+  return parseResult(matches);
+}
+
 /**
  * Gets the URL, site name, title, description, keywords, and author info out of the <head> meta tags from a given html string.
  * 1. Get the URL from the <meta property="og:url"> tag.
@@ -23,13 +121,16 @@ type Metadata = {
  * @returns A Metadata object with data from the HTML <head>
  */
 export default function getMetadata(html) {
-  // TODO: delete and replace this with your code
+  if (!_.isString(html)) {
+    html = "";
+  }
+
   return {
-    url: "https://www.example.com/something/to-test",
-    siteName: "Example Site Name",
-    title: "Example Title",
-    description: "Example description.",
-    keywords: ["example", "keywords"],
-    author: "Example Author",
+    url: getUrl(html),
+    siteName: getSiteName(html),
+    title: getTitle(html),
+    description: getDescription(html),
+    keywords: getKeywords(html),
+    author: getAuthor(html),
   };
 }
