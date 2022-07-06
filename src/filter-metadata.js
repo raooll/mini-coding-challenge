bae7cfae318dd1ce9c722b66921cfac4eff74df4
raooll <rahul.aug.1986@gmail.com>
2022-07-06 15:45:24 +0530

    complete filter metadata function

--- src/filter-metadata.js
+++ src/filter-metadata.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+
 /*
 type Metadata = {
   url: string | null;
@@ -8,6 +10,67 @@ type Metadata = {
   author: string | null;
 };
 */
+
+/**
+ * Removes special characters from a string
+ * @param {string} string - The string to remove special characters from
+ * @returns {string} - A string with no special characters
+ *
+ */
+function sanitize(string) {
+  return string.replace(/[^a-zA-Z0-9 ]/g, "");
+}
+
+/**
+ * Checks if metadata matches a query
+ * If the search query has multiple words,
+ * treat each word as a separate search term to filter by,
+ * in addition to gathering results from the overall query.
+ * If the search query has special characters,
+ * run the query filter with the special characters removed.
+ * @param {Metadata} metadata - A metadata object
+ * @param {string[]} queryWords - The array of search query string
+ * @returns {boolean} - true: if the metadata has query string, else false
+ */
+function contains(metadata, queryWords) {
+  if (_.isEmpty(queryWords)) {
+    return true;
+  }
+
+  const searchResultArray = Object.entries(metadata).map(([key, value]) => {
+    if (value == null) {
+      return false;
+    }
+
+    if (key === "keywords") {
+      return value.some((keyword) =>
+        queryWords.includes(keyword.toLowerCase())
+      );
+    } else {
+      return queryWords.some((word) =>
+        sanitize(value).toLowerCase().includes(word)
+      );
+    }
+  });
+
+  return searchResultArray.includes(true);
+}
+
+/**
+ * Prepares a list of all words combinations to match.
+ * @param {string} query : A string query
+ * @returns {string[]} : An array of all the query term from the query string
+ */
+function prepareQueryWords(query) {
+  let queryWords = query.split(" ").flatMap((w) => {
+    if (w.includes("-")) {
+      return w.split("-").concat(w.replace("-", ""));
+    } else {
+      return w;
+    }
+  });
+  return queryWords.map(sanitize).map(_.lowerCase);
+}
 
 /**
  * Filters the given Metadata array to only include the objects that match the given search query.
@@ -23,5 +86,11 @@ type Metadata = {
  */
 export default function filterMetadata(metadata, query) {
   // TODO: delete and replace this with your code
-  return metadata;
+
+  if (_.isArray(metadata) && _.isString(query)) {
+    const queryWords = prepareQueryWords(query);
+    return metadata.filter((mdata) => contains(mdata, queryWords));
+  } else {
+    return [];
+  }
 }
